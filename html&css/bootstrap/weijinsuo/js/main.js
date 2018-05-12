23aa21a1c4a8b67e4894ca2181304971cdfdbd72
Zsuz <Miszsuz@163.com>
2018-05-12 23:11:22 +0800

    新闻部分完成

---
+++ html&css/bootstrap/weijinsuo/js/main.js
@@ -0,0 +1,63 @@
+/*
+* @Author: Mizxinp
+* @Date:   2018-02-09 14:13:53
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-03-03 16:23:37
+*/
+$(function () {
+    function adaptScreen() {
+        //拿到窗口宽度
+        var windowWidth = $(window).width();
+        //判断是不是小图片
+        var isSmallScreen = windowWidth < 768;
+        $('#main_ad > .carousel-inner > .item').each(function (index, element) {
+            //这里要注意，拿到的是dom对象，要转换成jq对象
+            $element = $(element);
+            //得到图片的路径
+            var imgSrc = isSmallScreen ? $element.data('small-image') : $element.data('large-image');
+            //设置样式（之前一直没有图片出现，原因是设置样式时写成了$element.css("backgroundImage",imgSrc;
+            $element.css("backgroundImage", 'url("' + imgSrc + '")');
+            // 因为我们需要小图时 尺寸等比例变化，所以小图时我们使用img方式
+            if (isSmallScreen) {
+                $element.html('<img src="' + imgSrc + '" alt="" />');
+            } else {
+                $element.empty();
+            }
+        });
+    }
+
+    //当调整浏览器窗口的大小时，发生 resize 事件,未调整先让它出发一次
+    //$(window).on('resize', adaptScreen).trigger('adaptScreen');
+    adaptScreen();
+
+    $(window).on('resize', adaptScreen);
+
+    //特色推荐部分小工具提示初始化
+    $('[data-toggle="tooltip"]').tooltip()
+
+    /* 横向滚动条 */
+    //获取元素
+    var $elem = $('.nav-tabs')
+    //获取宽度
+    var width = 30;
+    $elem.children().each(function(index,element){
+        width += element.clientWidth;
+    })
+    /* 这样写窗口变大后也会有滚动条
+    $elem.css('width',width) */
+
+    //优化
+        if(width>$(window).width()){
+            $elem.css('width',width).parent().css('overflow','scroll');
+        } 
+    //
+
+
+    //新闻部分点击li相应的标题做出更改
+    var $newsTitle = $('.title a');
+    $newsTitle.on('click',function(){
+        var $this = $(this)
+        var title = $this.data('news-title');
+        $('.news-title').text(title);
+    })
+})
